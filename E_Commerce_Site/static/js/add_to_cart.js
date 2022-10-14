1d2d1b34bcd60582797a7cb2bf4d1499f90d8a35
alpha <alpha@DESKTOP-70NRL08>
2022-10-14 22:16:12 +0200

    first commit

--- E_Commerce_Site/static/js/add_to_cart.js
+++ E_Commerce_Site/static/js/add_to_cart.js
@@ -21,8 +21,9 @@ $(document).ready(function() {
 
 
 
-  
-    $("#add_to_cart").click(function(e){
+    // setInterval(function(){},5000)
+
+    $("#add_to_cart").on("click",function(e){
         e.preventDefault();
         var form=document.getElementById("quan_f");
         csrftoken = form.getElementsByTagName("input")[0].value; 
@@ -33,6 +34,7 @@ $(document).ready(function() {
             sizes:"",
             color:"",
             account:"",
+            url_html:"",
             set set_glob_cate(glob_cate_n){
                 this.glob_cate=glob_cate_n
             },
@@ -51,9 +53,13 @@ $(document).ready(function() {
             set set_account(account_n){
                 this.account=account_n
             },
-          
-
             
+            set set_url_html(html){
+                this.url_html=html
+            },
+            get get_url_html() {
+                return this.url_html;
+            },
             get get_glob_cate() {
                 return this.glob_cate;
             },
@@ -132,58 +138,85 @@ $(document).ready(function() {
                         $("#message_Add_to_cart").empty()
                     }
                     setTimeout(clear_my_func,4000)
+                    
+
+
+
+
+
                     var data = new FormData();
-                        data.append("cat_name",$("#category_name_global").html())
-                        data.append("child_name",$("#child_name_global").html())
-                        data.append("prod_name",$("#prod_name_global").html())
-                        data.append("size",checkboxes_sizes.toString())
-                        data.append("color",checkboxes_color.toString())
-                        data.append("price",$("#price").attr("name"))
-                        data.append("account",$("#account").attr("name"))
-                        data.append("quantity",$("#quan").val())
-    
-                    const options = {
-                        headers: {"X-CSRFToken" :csrftoken}
-                      };
+                    data.append("cat_name",$("#category_name_global").html())
+                    data.append("child_name",$("#child_name_global").html())
+                    data.append("prod_name",$("#prod_name_global").html())
+                    data.append("size",checkboxes_sizes.toString())
+                    data.append("color",checkboxes_color.toString())
+                    data.append("price",$("#price").attr("name"))
+                    data.append("account",$("#account").attr("name"))
+                    data.append("quantity",$("#quan").val())
 
-                      
-                    axios.post('add_to_cart',data,options)
-                      .then((response) => {
-                        products_count = $($.parseHTML(response.data)).filter("#products_count").html();
+                const options = {
+                    headers: {"X-CSRFToken" :csrftoken}
+                  };
+
+                axios.post('add_to_cart',data,options)
+                  .then((response) => {
+                    products_count = $($.parseHTML(response.data)).filter("#products_count").html();
+                    data_to_cart.set_url_html=response.data
+
+                    if(products_count!=0){
+                        $("#products_in_card").attr("class","word")
+                        $("#products_in_card").attr("hidden",false)
+                        $("#products_in_card").html(products_count)
+
+                    }else{
+                        $("#products_in_card").attr("class","")
+                        $("#products_in_card").attr("hidden",true);
+                    }
+
+                    $('#my_btn_iframe').popover({ 
                         
-
-                        if(products_count!=0){
-                            $("#products_in_card").attr("class","word")
-                            $("#products_in_card").attr("hidden",false)
-                            $("#products_in_card").html(products_count)
-
-                        }else{
-                            $("#products_in_card").attr("class","")
-                            $("#products_in_card").attr("hidden",true);
-                        }
-                        var myDefaultWhiteList = $.fn.tooltip.Constructor.Default.whiteList
-
-                        myDefaultWhiteList.iframe = ['style','onload',"contentWindow","class","width","height"]
-
-                        $('#my_btn_iframe').popover({ 
+                        title : 'My Checkout',
+                        html : true,
+                        placement : "bottom",
+                        container: 'body',
                         
-                            title : 'My Checkout',
-                            html : true,
-                            placement : "bottom",
-                            container: 'body',
-                            content: function() {
-                            html=response.data.toString()
-                                console.log(html)
-                                return "<iframe style='border:none;'  class='popover' onload='adjustPopover(&quot;.pop-bottom&quot; , this , html )'></iframe>";    
-                               
-                             }
-                         })
+            content: function() {
+                var myDefaultWhiteList = $.fn.tooltip.Constructor.Default.whiteList
+                myDefaultWhiteList.iframe = ['style','src','id','onload',"contentWindow","class","width","height"]
+                console.log(response.data.toString())
 
-                      }, (error) => {
+                 html=response.data.toString()
+                console.log(data_to_cart.get_url_html)
+                return "<iframe id='my_iframe' style='border:none;'  class='popover' onload='adjustPopover(&quot;.pop-bottom&quot; , this , data_to_cart.get_url_html )'></iframe>";    
+
+                   }})
+
+              
+                }).catch(
+                (error) => {
                         console.log(error);
-                      });
+                     }
+                )
 
 
-            
-                    }}}
-                    )})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+                    }}})})
